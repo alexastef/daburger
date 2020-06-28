697823b67d72e4c8a15de8e01506ed5686b823fe
Alexa Stefankiewicz <alexa.c.stef@gmail.com>
2020-06-28 16:25:25 -0700

    adding working server code

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 const express = require("express");
+const PORT = process.env.PORT || 8000;
+const app = express();
+const handlebars = require("express-handlebars");
+const routes = require("./controllers/burgers_controller.js");
+
+// Serve static content from public folder
+app.use(express.static("public"));
+
+// Parse as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set up handlebars
+app.set("view engine", "handlebars");
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+
+app.use(routes);
+
+// Start server and begin listening to client requests
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+})
+
+
